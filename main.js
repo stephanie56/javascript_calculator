edcb2209391bd5a9d33883a518b5aec0f4e0ff30
Stephanie Zeng <stephaniezeng521@gmail.com>
2016-12-17 19:04:01 -0500

    Added operation object template

--- main.js
+++ main.js
@@ -1,27 +1,40 @@
 /** JS CALCULATOR **/
 // User Story: I can add, subtract, multiply and divide two numbers.
-
 // User Story: I can clear the input field with a clear button.
-
 // User Story: I can keep chaining mathematical operations together until I hit the equal button, and the calculator will tell me the correct output.
 
 $(document).ready(function(){
-
-  var arg1, arg2, arg3;
-
+  var inpArr = [];
+  var secinpArr = [];
+  var firstNum;
+  var secNum;
+  var opObj = {
+    firstInp: null,
+    secInp: null,
+    operation: null
+  };
   $(".key").click(function(){
-    arg1 = $(this).attr("data-key");
-    console.log(arg1);
+    if($(this).hasClass("numkey")){
+      inpArr.push($(this).attr("data-key"));
+      firstNum = inpArr.join("");
+      opObj.firstInp = firstNum;
+    }
+    else {
+      opObj.operation = $(this).attr("data-key");
+      // if first arg is not empty, input second arg
+      if(opObj.firstInp !== null){
+        $(".key").click(function(){
+          secinpArr.push($(this).attr("data-key"));
+          secNum = secinpArr.join("");
+          opObj.secInp = secNum;
+        });
+      }
+    }
+    console.log(opObj);
   });
 
-  $(".key").click(function(){
-    arg2 = $(this).attr("data-key");
-    console.log(arg2);
-  });
+  //console.log(opObj);
 
-  $("li[data-key='equal']").click(function(){
-    console.log(add(arg1, arg2));
-  });
 });
 
 function add (a, b){
